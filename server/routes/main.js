f916e02e8f590a3f9b672d99bef3b27cdd9fc6db
LaRenzo <dlarenzo@outlook.com>
2024-01-30 09:58:21 -0700

    completed final section

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,15 +4,34 @@ const Post = require("../models/Post");
 
 //  HOMEPAGE
 router.get("/", async (req, res) => {
-  const locals = {
-    title: "NodeJS Blog",
-    description:
-      "A Blog template application that will be used for your own use.",
-  };
-
   try {
-    const data = await Post.find().sort({ title: "desc" });
-    res.render("index", { locals, data });
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
+    };
+
+    let perPage = 3;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { title: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    // Count is deprecated - please use countDocuments({}) instead
+    // const count = await Post.count();
+    const count = await Post.countDocuments({});
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+    const hasNextPagePlus = nextPage <= Math.ceil(count * perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+      prevPage: hasNextPagePlus ? page - 1 : null,
+    });
   } catch (error) {
     console.log(error);
   }
