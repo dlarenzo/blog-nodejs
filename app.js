2777ff43a5f18ff5a64e4a4544bc70136e072936
LaRenzo <dlarenzo@outlook.com>
2024-01-30 10:30:57 -0700

    starting part 3

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
 const expressLayouts = require("express-ejs-layouts");
 const express = require("express");
 const app = express();
-
 const PORT = 4000;
+
+const methodOverride = require("method-override");
+const session = require("express-session");
 
 const connectDB = require("./server/config/db");
 connectDB();
@@ -12,15 +16,25 @@ connectDB();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  })
+);
+
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 app.use("/", require("./server/routes/main"));
-
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
 
 app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
